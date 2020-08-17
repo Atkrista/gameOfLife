75809669465c0c33713f0a3f8dac36b43c5180c4
Atkrista <atulaniya1@gmail.com>
2020-08-17 09:25:16 +0545

    Sth is wrong

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from random import random
 import string
 
 #height and width of the board
-WIDTH = 10
+WIDTH = 20
 HEIGHT = 10
 
-def dead_state(w= WIDTH, h= HEIGHT):
-    new_arr = np.zeros(shape=(w,h),dtype = int)
+def dead_state(w= HEIGHT, h= WIDTH):
+    new_arr = np.zeros(shape=(w,h), dtype = int)
     return new_arr
 
 def random_state():
@@ -26,20 +26,64 @@ def random_state():
     return initial_state
 
 def render_state(state):
-    new_arr = np.zeros((len(state), len(state[0])), dtype = str)
+    new_arr = np.zeros(shape=(len(state), len(state[0])), dtype = str)
     for i in range(len(state)):
         for j in range(len(state[0])):
             if state[i, j] == 0:
-                np.put(new_arr[i], j, '.' )
+                new_arr[i][j] = '.'
             else:
-                np.put(new_arr[i], j, '#')             
+                new_arr[i][j] = '#'
     return new_arr
 
 
 
-def print_state(arr):
-    print(arr)
+def print_board(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            print('|' + arr[i][j] + '|', end ='')
+        print()        
+
+def update_state(initial_state):
+    new_state = initial_state
+    for i in range(len(new_state)):
+        for j range(len(new_state[0])):
+            if ()(update_cell_state(initial_state[i][j]) == True):
+                new_state[i][j] = 
+
+
+
+def update_cell_state(cell):
+    if cell == 1:
+        cell_alive = True
+        try:
+        sum =(cell[i-1][j-1]+ cell[i-1][j] + cell[i-1][j+1] + cell[i][j-1]+ cell[i][j] + cell[i][j+1] +cell[i+1][j-1]+ cell[i+1][j] + cell[i+1][j+1])
+        except IndexError:
+            cell[i][j] = 0
+        
+        if (cell_alive == True) AND (sum <= 1):
+            cell_next_state = False
+        elif (cell_alive == True) AND (sum == 2 OR sum == 3):
+            cell_next_state = True 
+        elif (cell_alive == True) AND (sum > 3):
+            cell_next_state = False
+    elif cell == 0:
+        cell_alive = False
+        try:
+        sum =(cell[i-1][j-1]+ cell[i-1][j] + cell[i-1][j+1] + cell[i][j-1]+ cell[i][j] + cell[i][j+1] +cell[i+1][j-1]+ cell[i+1][j] + cell[i+1][j+1])
+        except IndexError:
+            cell[i][j] = 0
+
+        if(cell_alive == False) AND (sum == 3):
+            cell_next_state = True        
     
+
+    return cell_next_state    
+
+
+
+
+
+
 
 board1 = random_state()
 board2 = dead_state()
@@ -47,6 +91,7 @@ board2 = dead_state()
 rendered1 = render_state(board1)
 rendered2 = render_state(board2)
 
-#print(rendered1)
-print(board2)
+print_board(rendered1)
 
+
+    
