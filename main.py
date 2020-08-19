8e16b03f480ffe1ec7217345dcd4db71a4c8636d
Atkrista <atkristak@gmail.com>
2020-08-19 06:57:58 +0545

    canged how is_alive() is implemented. Still states dont change

--- main.py
+++ main.py
@@ -12,21 +12,28 @@ def dead_state():
     list = np.zeros(shape = (HEIGHT, WIDTH), dtype = int)
     return list
 
-def is_alive(cell, i, j):
-    if cell[i][j] == 1:
-        return True
-    else:
-        return False
+#returns a randomised initial state of the gOfLife
 
 def initial_state():
+    #copies a initial dead state to modify
     initial = dead_state()
     for i in range(HEIGHT):
         for j in range(WIDTH):
-            if random.random() > 0.5 :
-                initial[i][j] = 1
+            if random.random() < 0.5 :
+                initial[i][j] = 0
             else: 
-                initial[i][j] = 0 
+                initial[i][j] = 1 
     return initial
+
+
+    #returns state of cell
+
+def is_alive(cell):
+    value = cell
+    if value == 1:
+        return True
+    else:
+        return False
 
 def change_state(cell, i, j):
     #if cell is Alive
@@ -40,16 +47,16 @@ def change_state(cell, i, j):
     except IndexError:
         state[i][j] = 0
 
-    if is_alive(state,i ,j) and (sum == 0 or sum == 1):
+    if is_alive(state[i][j]) and (sum == 0 or sum == 1):
         return True
 
-    elif is_alive(state,i ,j) and (sum == 2 or sum == 3):
+    elif is_alive(state[i][j]) and (sum == 2 or sum == 3):
         return False
 
-    elif is_alive(state,i ,j) and (sum > 3):    
+    elif is_alive(state[i][j]) and (sum > 3):    
         return True
 
-    elif not(is_alive(state,i ,j)) and (sum == 3) :
+    elif (is_alive(state[i][j]) == False) and (sum == 3) :
         return True
     else:
         return False
@@ -63,7 +70,7 @@ def next_state(previous_state):
             if(change_state(new_state, i, j) == True):
                 if new_state[i][j] == 1:
                     new_state[i][j] == 0
-                else:
+                elif new_state[i][j] == 0:
                     new_state[i][j] == 1        
 
     return new_state
@@ -78,9 +85,9 @@ def render_board(state):
     for i in range(HEIGHT):
         print("|", end = '')
         for j in range(WIDTH):
-            if is_alive(cell, i, j):
+            if is_alive(cell[i][j]):
                 print('# ', end = '')
-            elif not(is_alive(cell, i, j)):
+            elif not(is_alive(cell[i][j])):
                 print('  ', end = '')
         print("|", end ='')
         print()
@@ -95,19 +102,19 @@ if __name__ == '__main__':
 
     condition = True
 
-    render_board(old_state)
+    # render_board(old_state)
 
-    new = next_state(old_state)
+    # new = next_state(old_state)
 
-    render_board(new)
+    # render_board(new)
 
-    # #Run forever
-    # while(condition):
-    #     #print the state
-    #     render_board(old_state)
-    #     new_state = next_state(old_state)
-    #     time.sleep(0.5)
-    #     render_board(new_state)
-    #     old_state = new_state
+    #Run forever
+    while(condition):
+        #print the state
+        render_board(old_state)
+        new_state = next_state(old_state)
+        time.sleep(1)
+        render_board(new_state)
+        old_state = new_state
         
                 
